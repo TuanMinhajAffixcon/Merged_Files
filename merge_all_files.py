21d1d87b3949c4435e1f6a313c119512892f2925
TuanMinhajAffixcon <146834620+TuanMinhajAffixcon@users.noreply.github.com>
2024-06-19 19:54:10 +0530

    Update merge_all_files.py

--- merge_all_files.py
+++ merge_all_files.py
@@ -2,12 +2,13 @@ import streamlit as st
 import pandas as pd
 import os
 import zipfile
+import io
 
 uploaded_file = st.file_uploader("Upload ZIP file", type=["zip"])
-file_name=st.text_input("Enter file name:")
+file_name = st.text_input("Enter file name:")
 
-if file_name is not None:
-    if uploaded_file is not None:
+if file_name:
+    if uploaded_file:
         with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
             # Extract the contents of the ZIP file to a temporary directory
             temp_dir = 'temp_extracted_files'
@@ -26,8 +27,8 @@ if file_name is not None:
                         elif filename.endswith('.xlsx'):
                             data = pd.read_excel(filepath)
                     except Exception as e:
-                        print(f"Error reading file: {filename}. Skipping...")
-                        print(e)
+                        st.error(f"Error reading file: {filename}. Skipping...")
+                        st.error(e)
                         continue
                     data['File_Name'] = filename
                     merged_data = pd.concat([merged_data, data], ignore_index=True)
@@ -35,14 +36,19 @@ if file_name is not None:
         # Reset index starting from 1
         merged_data.index = merged_data.index + 1
 
-        merged_data.to_csv('opened_merged_data.csv')
+        # Use io.StringIO to handle CSV in memory
+        csv_buffer = io.StringIO()
+        merged_data.to_csv(csv_buffer, index=True, encoding='utf-8')
+        csv_buffer.seek(0)
 
         # Add a download button to download the generated CSV file
         st.download_button(
             label="Download CSV",
-            data=merged_data.to_csv(index=True).encode('utf-8'),
+            data=csv_buffer,
             file_name=f"{file_name}.csv",
             mime='text/csv'
         )
+    else:
+        st.warning('Please upload a ZIP file.')
 else:
     st.warning('Enter File Name')
