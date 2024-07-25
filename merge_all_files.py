7453a767e5ac8542563dac8affc4c9f89931b6e9
TuanMinhajAffixcon <146834620+TuanMinhajAffixcon@users.noreply.github.com>
2024-07-25 10:01:09 +0530

    Update merge_all_files.py

--- merge_all_files.py
+++ merge_all_files.py
@@ -33,10 +33,14 @@ if file_name is not None:
                         if filename.endswith('.csv'):
                             data = pd.read_csv(filepath, encoding='ISO-8859-1').fillna("")
                             data.columns = data.columns.str.upper()
+                            if 'EMAILADRESS' in data.columns and 'EMAILADDRESS' not in data.columns:
+                                data.rename(columns={'EMAILADRESS': 'EMAILADDRESS'}, inplace=True)
                             data = data[["ID","URN","TITLE","FIRSTNAME","SURNAME","EMAILADDRESS"]]
                         elif filename.endswith('.xlsx'):
                             data = pd.read_excel(filepath)
                             data.columns = data.columns.str.upper()
+                            if 'EMAILADRESS' in data.columns and 'EMAILADDRESS' not in data.columns:
+                                data.rename(columns={'EMAILADRESS': 'EMAILADDRESS'}, inplace=True)
                             data = data[["ID","URN","TITLE","FIRSTNAME","SURNAME","EMAILADDRESS"]]
                     except Exception as e:
                         print(f"Error reading file: {filename}. Skipping...")
